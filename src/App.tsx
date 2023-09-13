cd24f59c0c7492c8df510f2a5aa0ed6f8224fb50
Godbell <12191579@inha.edu>
2023-09-13 17:30:47 +0900

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './components/pages/Home';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/profile/:username"></Route>
           <Route path="/profile/:username/favorites"></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );

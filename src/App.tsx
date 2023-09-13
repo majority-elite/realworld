923831ecee2693d609591a007629c89edef4bf43
Godbell <12191579@inha.edu>
2023-09-13 17:28:42 +0900

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header isAuthenticated={false} />
         <Routes>
-          <Route path="/#/"></Route>
-          <Route path="/#/login"></Route>
-          <Route path="/#/register"></Route>
-          <Route path="/#/settings"></Route>
-          <Route path="/#/article/:slug"></Route>
-          <Route path="/#/editor/"></Route>
-          <Route path="/#/editor/:slug"></Route>
-          <Route path="/#/profile/:username"></Route>
-          <Route path="/#/profile/:username/favorites"></Route>
+          <Route path="/login"></Route>
+          <Route path="/register"></Route>
+          <Route path="/settings"></Route>
+          <Route path="/article/:slug"></Route>
+          <Route path="/editor/"></Route>
+          <Route path="/editor/:slug"></Route>
+          <Route path="/profile/:username"></Route>
+          <Route path="/profile/:username/favorites"></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Header.tsx
@@ -0,0 +1,95 @@
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+interface HeaderProps {
+  isAuthenticated: boolean;
+}
+
+export const Header = ({ isAuthenticated }: HeaderProps) => {
+  const location = useLocation();
+  const locationPathName = location.pathname;
+
+  useEffect(() => {}, [location]);
+
+  const navItemsAuthenticated = (
+    <>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${
+            locationPathName === '/editor' ? 'active' : ''
+          }`}
+          to="/editor"
+        >
+          <i className="ion-compose"></i> New Article{' '}
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${
+            locationPathName === '/settings' ? 'active' : ''
+          }`}
+          to="/settings"
+        >
+          <i className="ion-gear-a"></i> Settings{' '}
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${
+            /^\/profile.+$/.test(locationPathName) ? 'active' : ''
+          }`}
+          to="/profile/eric-simons"
+        >
+          <img src="" className="user-pic" />
+          Eric Simons
+        </Link>
+      </li>
+    </>
+  );
+
+  const navItemsUnauthenticated = (
+    <>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${
+            locationPathName === '/login' ? 'active' : ''
+          }`}
+          to="/login"
+        >
+          Sign in
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${
+            locationPathName === '/register' ? 'active' : ''
+          }`}
+          to="/register"
+        >
+          Sign up
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          conduit
+        </Link>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <Link
+              className={`nav-link ${locationPathName === '/' ? 'active' : ''}`}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          {isAuthenticated ? navItemsAuthenticated : navItemsUnauthenticated}
+        </ul>
+      </div>
+    </nav>
+  );
+};

fcd511506d3aebdd382d4379dc9ef637b4ecb79e
Godbell <12191579@inha.edu>
2023-09-15 23:58:16 +0900

    correct: use absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Home } from './components/pages/Home';
-import { Footer } from './components/Footer';
-import { Login } from './components/pages/Login';
-import { Register } from './components/pages/Register';
+import { Header } from '@/components/Header';
+import { Home } from '@/components/pages/Home';
+import { Footer } from '@/components/Footer';
+import { Login } from '@/components/pages/Login';
+import { Register } from '@/components/pages/Register';
 
 function App() {
   return (
